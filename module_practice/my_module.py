c04569e1842a27af0afaca2a91ec289f4bd7a2c8
riproskaie <68151998+riproskaie@users.noreply.github.com>
2024-01-16 13:36:08 +0900

    1

---
+++ module_practice/my_module.py
@@ -0,0 +1,14 @@
+def add(a, b):
+    return a + b
+
+
+def subtract(a, b):
+    return a - b
+
+
+class Multiplier:
+    def __init__(self):
+        pass
+
+    def do(self, a, b):
+        return a * b
